1892c29356b8c95b1671faac76725bc7f57f60a6
Titanic880 <Warframeia1501@gmail.com>
2021-02-14 18:10:09 -0700

    Added an enum and basic constructor to DataColl.cs

--- DataColl.cs
+++ DataColl.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace IotData
 {
-    class DataColl
+    public class DataColl
     {
+        /// <summary>
+        /// The Type of device
+        /// </summary>
+        public enum DataType
+        {
+            GPS,
+            Electric,
+            Water,
+            Gas
+        }
+
+        public DataColl(string Name, DataType dt, int Measure1, int MeasureValue1, int Measure2, int MeasureValue2)
+        {
+
+        }
     }
 }
