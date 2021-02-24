57a620e46521548f6f2120b59b8eff885438ea85
Titanic880 <Warframeia1501@gmail.com>
2021-02-24 10:34:17 -0700

    Deleted DataColl as it was legacy, New file is Components\DataSchema.cs

--- Components/DataSchema.cs
+++ Components/DataSchema.cs
@@ -6,8 +6,23 @@ using System.Threading.Tasks;
 
 namespace IotData.Components
 {
+    ///The data Schema for generating the Data used
     public class DataSchema
     {
-        ///The data Schema for generating the Data used
-    }
+        /// <summary>
+        /// The Type of device
+        /// </summary>
+        public enum DataType
+        {
+            GPS,
+            Electric,
+            Water,
+            Gas
+        }
+
+        public DataSchema(string Name, DataType dt, int Measure1, int MeasureValue1, int Measure2, int MeasureValue2)
+        {
+
+        }
+}
 }
