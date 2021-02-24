d38b269ded31d5fa0f23b22a082fee01e5ac4aae
Titanic880 <Warframeia1501@gmail.com>
2021-02-24 12:23:35 -0700

    Moved DataInfo class to its own file

---
+++ Components/DataInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+
+namespace IotData.Components
+{/// <summary>
+ /// Handles and stores environment variables
+ /// </summary>
+    public class DataInfo
+    {
+        public readonly string Datapath;
+        public readonly string DataFile;
+        public readonly int DeviceCount;
+        public readonly int dataDelay;
+
+        public static readonly int CPUCORES = Environment.ProcessorCount - 2;
+
+        public DataInfo()
+        {
+            Datapath = ConfigurationManager.AppSettings.Get("fileDirectory");
+            DataFile = ConfigurationManager.AppSettings.Get("filePath");
+            DeviceCount = int.Parse(ConfigurationManager.AppSettings.Get("deviceCount"));
+            dataDelay = int.Parse(ConfigurationManager.AppSettings.Get("dataDelayMillis"));
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -14,23 +14,6 @@ using System.Windows.Forms;
 
 namespace IotData
 {
-    public class DataInfo
-    {
-        public readonly string Datapath;
-        public readonly string DataFile;
-        public readonly int DeviceCount;
-        public readonly int dataDelay;
-
-        public static readonly int CPUCORES = Environment.ProcessorCount - 2;
-
-        public DataInfo()
-        {
-            Datapath = ConfigurationManager.AppSettings.Get("fileDirectory");
-            DataFile = ConfigurationManager.AppSettings.Get("filePath");
-            DeviceCount = int.Parse(ConfigurationManager.AppSettings.Get("deviceCount"));
-            dataDelay = int.Parse(ConfigurationManager.AppSettings.Get("dataDelayMillis"));
-        }
-    }
     public partial class Form1 : Form
     {
 
