0fc1d6b3147e47bdc346d010ef1999df6bc48e21
Titanic880 <Warframeia1501@gmail.com>
2021-02-27 21:14:44 -0700

    Changed the name and calculation of the Thread Count

--- Components/DataInfo.cs
+++ Components/DataInfo.cs
@@ -7,12 +7,27 @@ namespace IotData.Components
  /// </summary>
     public static class DataInfo
     {
+        /// <summary>
+        /// The full file path for the data File
+        /// </summary>
         public readonly static string Datapath = ConfigurationManager.AppSettings.Get("fileDirectory");
+        /// <summary>
+        /// The file name
+        /// </summary>
         public readonly static string DataFile = ConfigurationManager.AppSettings.Get("filePath");
+        /// <summary>
+        /// The amount of Devices
+        /// </summary>
         public readonly static int DeviceCount= int.Parse(ConfigurationManager.AppSettings.Get("deviceCount"));
+        /// <summary>
+        /// The delay of data
+        /// </summary>
         public readonly static int dataDelay = int.Parse(ConfigurationManager.AppSettings.Get("dataDelayMillis"));
 
-        public static readonly int CPUCORES = Environment.ProcessorCount - 2;
+        /// <summary>
+        /// The number of threads avaliable for background workers
+        /// </summary>
+        public static readonly int ThreadsForWorkers = Environment.ProcessorCount - 4;
 
         /// <summary>
         /// The Type of device
